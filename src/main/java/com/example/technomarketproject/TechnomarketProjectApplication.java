08f5c3062643b8f45dd28a894c30194a8d96e7ed
Yoan Pavlov <ioan_2003@abv.bg>
2023-04-13 14:36:38 +0300

    Adding Model Mapper to the project

--- src/main/java/com/example/technomarketproject/TechnomarketProjectApplication.java
+++ src/main/java/com/example/technomarketproject/TechnomarketProjectApplication.java
@@ -1,7 +1,9 @@
 package com.example.technomarketproject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TechnomarketProjectApplication {
@@ -9,5 +11,11 @@ public class TechnomarketProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(TechnomarketProjectApplication.class, args);
     }
-
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
+
+
+
