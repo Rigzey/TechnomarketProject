6b6fdae2972cf08b746abd78b932eccc979aa33a
mariomanev <mario.g.manev@gmail.com>
2023-04-15 02:52:58 +0300

    Add Review functionality added.

--- src/main/java/com/example/technomarketproject/controller/controllers/ReviewController.java
+++ src/main/java/com/example/technomarketproject/controller/controllers/ReviewController.java
@@ -1,7 +1,12 @@
 package com.example.technomarketproject.controller.controllers;
 
 import com.example.technomarketproject.controller.services.ReviewService;
+import com.example.technomarketproject.model.DTOs.AddReviewDTO;
+import com.example.technomarketproject.model.entities.Review;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +15,10 @@ public class ReviewController extends GeneralController {
     @Autowired
     private ReviewService reviewService;
 
+    @PostMapping("/reviews")
+    public Review add(@RequestBody AddReviewDTO dto, HttpSession s) {
+        int id = findSessionLoggedId(s);
+        return reviewService.addReview(dto, id);
+    }
 
 }
--- src/main/java/com/example/technomarketproject/controller/services/ReviewService.java
+++ src/main/java/com/example/technomarketproject/controller/services/ReviewService.java
@@ -1,7 +1,33 @@
 package com.example.technomarketproject.controller.services;
 
+import com.example.technomarketproject.model.DTOs.AddReviewDTO;
+import com.example.technomarketproject.model.entities.Review;
+import com.example.technomarketproject.model.entities.User;
+import com.example.technomarketproject.model.exceptions.BadRequestException;
+import com.example.technomarketproject.model.exceptions.FileNotFoundException;
+import com.example.technomarketproject.model.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ReviewService extends AbstractService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    public Review addReview(AddReviewDTO dto, int id) {
+        Optional<User> opt = userRepository.findById(id);
+        if(opt.isEmpty()){
+            throw new FileNotFoundException("User with id " + id + " not found!");
+        }
+        if (dto.getRating() <= 0 || dto.getRating() > 10) {
+            throw new BadRequestException("Product rating should be between 1 and 10.");
+        }
+        Review r = mapper.map(dto, Review.class);
+        r.setUser(opt.get());
+        reviewRepository.save(r);
+        return r;
+    }
 }
