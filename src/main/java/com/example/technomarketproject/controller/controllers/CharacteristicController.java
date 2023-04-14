bb81de7a17d60fa4967cdcac32cf55b688d13e3e
Yoan Pavlov <ioan_2003@abv.bg>
2023-04-14 22:33:52 +0300

    Added characteristics with adding and deleting functionalities

---
+++ src/main/java/com/example/technomarketproject/controller/controllers/CharacteristicController.java
@@ -0,0 +1,28 @@
+package com.example.technomarketproject.controller.controllers;
+
+import com.example.technomarketproject.controller.services.CharacteristicService;
+import com.example.technomarketproject.model.DTOs.AddNewCharacteristicDTO;
+import com.example.technomarketproject.model.entities.Characteristic;
+import com.sun.net.httpserver.HttpsServer;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CharacteristicController extends GeneralController{
+    @Autowired
+    private CharacteristicService characteristicService;
+
+    @PostMapping("/characteristics")
+    public Characteristic add(@RequestBody AddNewCharacteristicDTO dto, HttpSession s){
+        int id = findSessionLoggedId(s);
+        return characteristicService.addCharacteristic(dto, id);
+    }
+
+    @DeleteMapping("/characteristics/{cId}")
+    public void delete(@PathVariable int cId, HttpSession s){
+        int loggedId = findSessionLoggedId(s);
+        characteristicService.deleteCharacteristic(cId, loggedId);
+    }
+
+}
