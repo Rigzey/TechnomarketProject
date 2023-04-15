79d0da4bf651cdddf7a3c14ca0bd76d60fa08401
mariomanev <mario.g.manev@gmail.com>
2023-04-15 04:52:32 +0300

    Add Product to Shopping Cart functionality added.

--- src/main/java/com/example/technomarketproject/controller/controllers/ShoppingCartController.java
+++ src/main/java/com/example/technomarketproject/controller/controllers/ShoppingCartController.java
@@ -1,7 +1,12 @@
 package com.example.technomarketproject.controller.controllers;
 
 import com.example.technomarketproject.controller.services.ShoppingCartService;
+import com.example.technomarketproject.model.DTOs.AddToShoppingCartDTO;
+import com.example.technomarketproject.model.entities.ShoppingCart;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,10 @@ public class ShoppingCartController extends GeneralController {
 
     @Autowired
     private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/cart")
+    private ShoppingCart addProduct(@RequestBody AddToShoppingCartDTO dto, HttpSession s) {
+        int id = findSessionLoggedId(s);
+        return shoppingCartService.addProduct(dto, id);
+    }
 }
--- src/main/java/com/example/technomarketproject/controller/services/ShoppingCartService.java
+++ src/main/java/com/example/technomarketproject/controller/services/ShoppingCartService.java
@@ -1,12 +1,29 @@
 package com.example.technomarketproject.controller.services;
 
+import com.example.technomarketproject.model.DTOs.AddToShoppingCartDTO;
+import com.example.technomarketproject.model.entities.ShoppingCart;
+import com.example.technomarketproject.model.entities.User;
+import com.example.technomarketproject.model.exceptions.FileNotFoundException;
 import com.example.technomarketproject.model.repositories.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ShoppingCartService extends AbstractService {
 
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
+
+    public ShoppingCart addProduct(AddToShoppingCartDTO dto, int id) {
+        Optional<User> opt = userRepository.findById(id);
+        if(opt.isEmpty()){
+            throw new FileNotFoundException("User with id " + id + " not found!");
+        }
+        ShoppingCart sc = mapper.map(dto, ShoppingCart.class);
+        sc.setUser(opt.get());
+        shoppingCartRepository.save(sc);
+        return sc;
+    }
 }
