d3dcaeef18ee27e40630b078a10eb2dd33a4d332
mariomanev <mario.g.manev@gmail.com>
2023-04-16 14:46:09 +0300

    Fixes performed on Search History Repository and Entity.

--- src/main/java/com/example/technomarketproject/controller/controllers/UserController.java
+++ src/main/java/com/example/technomarketproject/controller/controllers/UserController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Set;
 
 @RestController
